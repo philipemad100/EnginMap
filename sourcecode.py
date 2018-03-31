c340b4fd6ab6059a12bf411c20cc00d3f756fc7b
PhilipEmad <philip.emad.ghobrial@gmail.com>
2018-03-31 18:46:14 +0200

    Adding acceleration

--- sourcecode.py
+++ sourcecode.py
@@ -5,12 +5,12 @@ def taskCalculations():
     Pe=[23.2,49.8,76.75,100.9,119.1,129] #EnginePower
     Ne=[1000,2000,3000,4000,5000,6000]   #EngineTurns(rpm)
     Te=[]                                #EngineTorque
-    V1=[];FA1=[];TE1=[];DF1=[]           #V=Velocity
-    V2=[];FA2=[];TE2=[];DF2=[]           #TE=TractiveEffort
-    V3=[];FA3=[];TE3=[];DF3=[]           #DynamicFactor
-    V4=[];FA4=[];TE4=[];DF4=[]
-    V5=[];FA5=[];TE5=[];DF5=[]
-    V6=[];FA6=[];TE6=[];DF6=[]
+    V1=[];FA1=[];TE1=[];DF1=[];a1=[]     #V=Velocity
+    V2=[];FA2=[];TE2=[];DF2=[];a2=[]     #TE=TractiveEffort
+    V3=[];FA3=[];TE3=[];DF3=[];a3=[]     #DF=DynamicFactor
+    V4=[];FA4=[];TE4=[];DF4=[];a4=[]     #a=Accleration
+    V5=[];FA5=[];TE5=[];DF5=[];a5=[]
+    V6=[];FA6=[];TE6=[];DF6=[];a6=[]
     
     GVW=24000; Cd=0.38
     #GVW=  rossVehicleMass; Cd= DragCoefficient
@@ -27,6 +27,7 @@ def taskCalculations():
         FA1.insert(i,(round((0.5*p*Cd*Af*((5/18)*V1[i])**2),2)))
         TE1.insert(i,(round((Te[i]*z*ig[0]*iF)/rw,2)))
         DF1.insert(i,(round((TE1[i]-FA1[i])/GVW,3)))
+        a1.insert(i,round(TE1[i]/(GVW/9.8),2))
     print(V1);print(FA1);print(TE1);print(DF1)
     print("-------------------")
     for i in range(len(Ne)): #Second gear
@@ -34,6 +35,7 @@ def taskCalculations():
         FA2.insert(i,(round((0.5*p*Cd*Af*((5/18)*V2[i])**2),2)))
         TE2.insert(i,(round((Te[i]*z*ig[1]*iF)/rw,2)))
         DF2.insert(i,(round((TE2[i]-FA2[i])/GVW,3)))
+        a2.insert(i,round(TE2[i]/(GVW/9.8),2))
     print(V2);print(FA2);print(TE2);print(DF2)
     print("---------------------")
     for i in range(len(Ne)): #Third gear
@@ -41,6 +43,7 @@ def taskCalculations():
         FA3.insert(i,(round((0.5*p*Cd*Af*((5/18)*V3[i])**2),2)))
         TE3.insert(i,(round((Te[i]*z*ig[2]*iF)/rw,2)))
         DF3.insert(i,(round((TE3[i]-FA3[i])/GVW,3)))
+        a3.insert(i,round(TE3[i]/(GVW/9.8),2))
     print(V3);print(FA3);print(TE3);print(DF3)
     print("---------------------")
     for i in range(len(Ne)): #Fourth gear
@@ -48,6 +51,7 @@ def taskCalculations():
         FA4.insert(i,(round((0.5*p*Cd*Af*((5/18)*V4[i])**2),2)))
         TE4.insert(i,(round((Te[i]*z*ig[3]*iF)/rw,2)))
         DF4.insert(i,(round((TE4[i]-FA4[i])/GVW,3)))
+        a4.insert(i,round(TE4[i]/(GVW/9.8),2))
     print(V4);print(FA4);print(TE4);print(DF4)
     print("---------------------")
     for i in range(len(Ne)): #fifth gear
@@ -55,6 +59,7 @@ def taskCalculations():
         FA5.insert(i,(round((0.5*p*Cd*Af*((5/18)*V5[i])**2),2)))
         TE5.insert(i,(round((Te[i]*z*ig[4]*iF)/rw,2)))
         DF5.insert(i,(round((TE5[i]-FA5[i])/GVW,3)))
+        a5.insert(i,round(TE5[i]/(GVW/9.8),2))
     print(V5);print(FA5);print(TE5);print(DF5)
     print("---------------------")
     for i in range(len(Ne)): #sixth gear
@@ -62,6 +67,7 @@ def taskCalculations():
         FA6.insert(i,(round((0.5*p*Cd*Af*((5/18)*V6[i])**2),2)))
         TE6.insert(i,(round((Te[i]*z*ig[5]*iF)/rw,2)))
         DF6.insert(i,(round((TE6[i]-FA6[i])/GVW,3)))
+        a6.insert(i,round(TE6[i]/(GVW/9.8),2))
     print(V6);print(FA6);print(TE6);print(DF6)
 
 
